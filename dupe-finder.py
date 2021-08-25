58558e5ddcdeb7e3750356137d73b8cd227ea7b5
Kevin Wood <endotronic@gmail.com>
2021-08-24 20:40:32 -0700

    Support reading sizes and hashes from a file

--- dupe-finder.py
+++ dupe-finder.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from collections import defaultdict
 import hashlib
-from os import listdir, path
+from os import listdir, path, sep
 import sys
 from typing import Dict, List, Generator, Optional, Sequence, Set, Text, Tuple
 
@@ -46,8 +46,8 @@ class File:
 class Directory:
     name = attr.ib(type=Text)
     absolute_path = attr.ib(type=Text)
-    files = attr.ib(type=Sequence[File])
-    subdirectories = attr.ib(type=Sequence["Directory"])
+    files = attr.ib(type=List[File])
+    subdirectories = attr.ib(type=List["Directory"])
     contained_hashes_cache = attr.ib(type=Optional[Set[int]], default=None)
     parent_dir = attr.ib(type=Optional["Directory"], default=None)
     entirely_duplicated_cache = attr.ib(type=Optional[bool], default=None)
@@ -102,7 +102,7 @@ class Directory:
         return False
 
     @classmethod
-    def populate(cls, name: Text, absolute_path: Text) -> "Directory":
+    def populate(cls, absolute_path: Text, name: Optional[Text] = None) -> "Directory":
         files = []  # type: List[File]
         subdirectories = []  # type: List[Directory]
 
@@ -110,18 +110,51 @@ class Directory:
         for fs_item in fs_items:
             item_path = path.join(absolute_path, fs_item)
             if path.isdir(item_path):
-                subdir = cls.populate(fs_item, item_path)
+                subdir = cls.populate(item_path, fs_item)
                 subdirectories.append(subdir)
             else:
-                file = File.populate(fs_item, item_path)
+                file = File.populate(item_path, fs_item)
                 files.append(file)
 
-        dir = Directory(name, absolute_path, files, subdirectories)
+        dir_name = name or path.dirname(absolute_path)
+        dir = Directory(dir_name, absolute_path, files, subdirectories)
         for file in files:
             file.parent_dir = dir
         for subdir in subdirectories:
             subdir.parent_dir = dir
         return dir
+
+    @classmethod
+    def populate_from_records(
+        cls, absolute_path: Text, sizes_file: Text, hashes_file: Text
+    ) -> "Directory":
+        dir_name = path.dirname(absolute_path)
+        root_dir = Directory(dir_name, absolute_path, [], [])
+        current_dir = root_dir
+
+        hashes = {}  # Dict[Text, int]
+        with open(hashes_file) as file_in:
+            for line in file_in:
+                hex_digest, _, file_path = line.strip().partition(" ")
+                norm_path = path.normpath(file_path)
+                hashes[norm_path] = int(hex_digest, 16)
+
+        with open(sizes_file) as file_in:
+            for line in file_in:
+                size_str, _, file_path = line.strip().partition(" ")
+                norm_path = path.normpath(file_path)
+                assert norm_path in hashes, "Hash missing for file " + norm_path
+
+                dir_path, filename = path.split(norm_path)
+                file = File(filename, norm_path, 0)
+                file.size = int(size_str)
+                file.md5_hash_cache = hashes[norm_path]
+                if path.normpath(dir_path) != current_dir.absolute_path:
+                    current_dir = root_dir.recursive_make(dir_path)
+                current_dir.files.append(file)
+                file.parent_dir = current_dir
+
+        return root_dir
 
     def get_files_recursive(self) -> Generator[File, None, None]:
         for file in self.files:
@@ -136,11 +169,48 @@ class Directory:
             for ancestor in self.parent_dir.get_parents_recursive():
                 yield ancestor
 
+    def recursive_make(self, full_path: Text) -> "Directory":
+        if self.absolute_path == full_path:
+            return self
 
-def file_dupes(root_path: Text) -> None:
+        normalized_path = path.normpath(full_path)
+        norm_path_components = list(filter(None, normalized_path.split(sep)))
+        this_path_components = list(filter(None, self.absolute_path.split(sep)))
+        assert (
+            norm_path_components[0 : len(this_path_components)] == this_path_components
+        )
+        missing_components = norm_path_components[len(this_path_components) :]
+        return self.recursive_make_with_components(missing_components)
+
+    def recursive_make_with_components(
+        self, missing_components: List[Text]
+    ) -> "Directory":
+        if len(missing_components) == 0:
+            return self
+
+        for subdirectory in self.subdirectories:
+            if subdirectory.name == missing_components[0]:
+                return subdirectory.recursive_make_with_components(
+                    missing_components[1:]
+                )
+
+        subdir_name = missing_components[0]
+        subdir_path = path.join(self.absolute_path, subdir_name)
+        new_dir = Directory(subdir_name, subdir_path, [], [])
+        self.subdirectories.append(new_dir)
+        new_dir.parent_dir = self
+        return new_dir.recursive_make_with_components(missing_components[1:])
+
+
+def file_dupes(
+    root_path: Text, sizes_file: Optional[Text], hashes_file: Optional[Text]
+) -> None:
     absolute_path = path.abspath(root_path)
-    dir_name = path.dirname(absolute_path)
-    root_dir = Directory.populate(dir_name, absolute_path)
+
+    if sizes_file and hashes_file:
+        root_dir = Directory.populate_from_records(root_path, sizes_file, hashes_file)
+    else:
+        root_dir = Directory.populate(absolute_path)
 
     # Group files by size
     files_by_size = defaultdict(list)
@@ -186,34 +256,6 @@ def file_dupes(root_path: Text) -> None:
             )
         )
 
-    """
-    # Process directories
-    for directory in dirs_with_dupes.values():
-        for file in directory.files:
-            if file.hash:
-                directory.contained_hashes.add(file.hash)
-                if file.duplicates:
-                    if all([f.parent_dir == directory for f in file.duplicates]):
-                        directory.entirely_duplicated = False
-            else:
-                directory.entirely_duplicated = False
-        if directory.entirely_duplicated is None and not directory.subdirectories:
-            directory.entirely_duplicated = True
-
-    # Print entirely duplicated directories
-    directories_and_sizes = list()  # type: List[Tuple[int, Directory]]
-    for directory in dirs_with_dupes.values():
-        if directory.entirely_duplicated:
-            total_size = sum([f.size for f in directory.files])
-            directories_and_sizes.append((total_size, directory))
-    for total_size, directory in sorted(directories_and_sizes, reverse=True):
-        print(
-            "{} entirely duplicated ({} bytes)".format(
-                directory.absolute_path, total_size
-            )
-        )
-    """
-
     # Print entirely duplicated directories
     print("--- DIRECTORIES ---")
     directories_and_sizes = list()  # type: List[Tuple[int, Directory]]
@@ -224,13 +266,33 @@ def file_dupes(root_path: Text) -> None:
             total_size = sum([f.size for f in directory.get_files_recursive()])
             directories_and_sizes.append((total_size, directory))
     for total_size, directory in sorted(directories_and_sizes, reverse=True):
-        print(
-            "{} entirely duplicated ({} bytes)".format(
-                directory.absolute_path, total_size
+        if total_size:
+            print(
+                "{} entirely duplicated ({} bytes)".format(
+                    directory.absolute_path, total_size
+                )
             )
-        )
+        else:
+            num_files = len(list(directory.get_files_recursive()))
+            print(
+                "{} entirely duplicated ({} files)".format(
+                    directory.absolute_path, num_files
+                )
+            )
 
 
 if __name__ == "__main__":
-    assert len(sys.argv) == 2, "Usage: dupe-finder.py <top-level dir>"
-    file_dupes(sys.argv[1])
+    assert len(sys.argv) in (
+        2,
+        4,
+    ), "Usage: dupe-finder.py <top-level dir> <sizes file> <hashes file>"
+    # Sizes `file: find / -type f -exec du -b {} \; > sizes_file.txt `
+    # Hashes file: `file: find / -type f -exec md5sum {} \; > hashes_file.txt `
+    root_dir = sys.argv[1]
+
+    hashes_file = sizes_file = None
+    if len(sys.argv) == 4:
+        sizes_file = sys.argv[2]
+        hashes_file = sys.argv[3]
+
+    file_dupes(root_dir, sizes_file, hashes_file)
